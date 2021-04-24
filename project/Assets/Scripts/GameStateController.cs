e22f0fe6c053886ad5763423cbf82de8cbcc3a19
Carles Perez Onielfa <98perezcarles@gmail.com>
2021-04-24 19:37:10 +0200

    UI progress

--- project/Assets/Scripts/GameStateController.cs
+++ project/Assets/Scripts/GameStateController.cs
@@ -9,10 +9,12 @@ public class GameStateController : MonoBehaviour
 
     [SerializeField] int initialGold;
     [SerializeField] int initialTurns;
+    [SerializeField] int initialDebt;
     [SerializeField] IntVariable gameState;
     [SerializeField] IntVariable gold;
+    [SerializeField] IntVariable debt;
     [SerializeField] IntVariable totalTurns;
-    [SerializeField] IntVariable remainingTurns;
+    [SerializeField] IntVariable currentTurn;
     
     // Start is called before the first frame update
     private void Awake()
@@ -22,8 +24,9 @@ public class GameStateController : MonoBehaviour
     void Start()
     {
         gold.Value = initialGold;
+        debt.Value = initialDebt;
         totalTurns.Value = initialTurns;
-        remainingTurns.Value = initialTurns;
+        currentTurn.Value = 0;
     }
 
     public void OnStatChanged(IntPair state)
@@ -31,9 +34,9 @@ public class GameStateController : MonoBehaviour
         //If previous state was spin or movement we advance one turn
         if(state.Item2 == (int)GameState.SPIN || state.Item2 == (int)GameState.MOVEMENT)
         {
-            remainingTurns.Value -= 1;
+            currentTurn.Value += 1;
         }
-        if(remainingTurns.Value == 0)
+        if(currentTurn.Value == 0)
         {
             gameState.Value = (int)GameState.PAY;
         }
--- project/Assets/Scripts/UtilitiesUI/ProgressBar.cs
+++ project/Assets/Scripts/UtilitiesUI/ProgressBar.cs
@@ -3,22 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityAtoms.BaseAtoms;
+using TMPro;
 public class ProgressBar : MonoBehaviour
 {
+    [SerializeField] bool vertical;
     [SerializeField] RectTransform maskTransform;
     [SerializeField] IntVariable maxValue;
-    float maxWidth;
+    [SerializeField] TextMeshProUGUI textMaxValue;
+    [SerializeField] TextMeshProUGUI textCurrentValue;
+
+    float maxSize;
 
     void Start()
     {
-        maxWidth = GetComponent<RectTransform>().rect.width;
+        if(vertical)
+            maxSize = GetComponent<RectTransform>().rect.height;
+        else
+            maxSize = GetComponent<RectTransform>().rect.width;
     }
     public void OnValueChanged(IntPair values)
     {
-        float step = maxWidth / maxValue.Value;
+        float step = maxSize / maxValue.Value;
 
-        LeanTween.value((maxValue.Value - values.Item2) * step, (maxValue.Value - values.Item1) * step, 1f).
-            setOnUpdate((float v) => maskTransform.sizeDelta = new Vector2(v, maskTransform.sizeDelta.y)).
+        LeanTween.value(values.Item2 * step, values.Item1 * step, 1f).
+            setOnUpdate((float v) => UpdateSize(v)).
             setEaseOutCubic();
+    }
+    public void UpdateSize(float value)
+    {
+        if (vertical)
+            maskTransform.sizeDelta = new Vector2(maskTransform.sizeDelta.x, value);
+        else
+            maskTransform.sizeDelta = new Vector2(value, maskTransform.sizeDelta.y);
+    }
+    public void UpdateTextCurrentValue(IntPair values)
+    {
+        LeanTween.value(values.Item2, values.Item1, 0.5f).
+            setOnUpdate((float val) => textCurrentValue.text = ((int)val).ToString());
+    }
+    public void UpdateTextMaxValue(int value)
+    {
+        textMaxValue.text = value.ToString();
     }
 }
